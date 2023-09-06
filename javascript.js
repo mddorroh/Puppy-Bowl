66970fb7ffc66f805a98db9589cdff1e7c109247
mddorroh <mddorroh@gmail.com>
2023-09-05 21:50:06 -0400

    third commit

--- javascript.js
+++ javascript.js
@@ -5,18 +5,16 @@ const fetchPlayers = async()=> {
     const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2307-ftb-et-web-ft/players');
     const json = await response.json();
     const players = json.data;
-    const playersArray = Object.values(players)
-   const html = playersArray.map( player => {
-    return `
-    <li>
-    ${ player.name }
-    </li>
-  `;
-  }).join('');
+  console.log(players);
+    const html = players.players.map( player => {
+        return `
+        <li>
+        ${ player.name }
+        </li>
+    `;
+    }).join('');
   ul.innerHTML = html;
   console.log(html);
-
-  console.log(playersArray)
 };
 
 fetchPlayers();
